1f0e7a5ea230fb798190ab4ec8b0f57d6c527d01
yongkyuns <shinMac@SHINs-MacBook-Pro.local>
2017-08-27 14:36:31 -0400

    Initial Commit

---
+++ temp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  temp
+//
+//  Created by YONGKYUN SHIN on 27/08/2017.
+//  Copyright © 2017 Yong Kyun Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
